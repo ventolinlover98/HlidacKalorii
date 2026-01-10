748f6f2fe2b8ed66a55c194071dfcddcd2045c6e
velikovte <tevelik1@gmail.com>
2026-01-10 18:34:22 +0100

    Další úprava css a html, začátky definování app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+class Meal {
+    constructor (name, calories) {
+        this.id = Date.now().toString();
+        this.name = name;
+        this.calories = calories;
+    }
+}
+
+class Workout {
+    constructor (name, calories) {
+        this.id = Date.now().toString;
+        this.name = name;
+        this.calories = calories;
+    }
+}
+
+
+
+const app = new App();
